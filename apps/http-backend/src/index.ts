626625815c4f7487e47c3b360d9b1a2c76c3e40f
Joy0810 <joynirantar2005@gmail,com>
2025-09-02 21:39:15 +0530

    basic http-backend middleware setup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,27 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { middleware } from "./middleware";
+
+dotenv.config();
+
+const JWT_SECRET=process.env.JWT_SECRET;
 
 const app=express();
 
-app.listen(3000);
+app.put("/signup",(req,res)=>{
+
+})
+
+app.put("/signin",(req,res)=>{
+    const userId=1;
+    const token=jwt.sign({
+        userId
+    },JWT_SECRET as string)
+})
+
+app.put("/room",middleware,(req,res)=>{
+    
+})
+
+app.listen(3001);
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import jwt  from "jsonwebtoken";
+import dotenv from "dotenv";
+import { JwtPayload } from "jsonwebtoken";
+
+dotenv.config();
+
+const JWT_SECRET=process.env.JWT_SECRET;
+
+declare global{
+    namespace Express{
+        export interface Request{
+            userId?:string
+        }
+    }
+}
+
+interface IJwtPayload extends JwtPayload{
+    userId?:string
+}
+
+export function middleware(req:Request,res:Response,next:NextFunction){
+    const token=req.headers["authorization"]??"";
+
+    const decoded=jwt.verify(token,JWT_SECRET as string) as IJwtPayload;
+
+    if(decoded){
+        req.userId=decoded.userId as string;
+        next();
+    }
+    else{
+        res.status(403).json({
+            message:"Unauthorized"
+        })
+    }
+}
